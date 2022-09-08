2a48d98f3c75ee08353899e360f0c99d152efe20
Krishnaveeran <dhlipkrishna@gmail.com>
2022-09-08 19:32:17 +0530

    add

---
+++ Example.java
@@ -0,0 +1,16 @@
+public class Example
+{
+
+public static void main(String [] args) 
+{
+int x=10;
+int y=20;
+int addition=add(x,y);
+System.out.println("addition of two number is:"+addition);
+}
+
+public static int add(int a,int b) //a & b are parameter (this is method definition
+{
+return a+b;
+}
+}
