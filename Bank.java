de99f04b741a7c1a74d5f02357018d174bb6596f
Krishnaveeran <dhlipkrishna@gmail.com>
2022-09-17 09:13:24 +0530

    Final keyword base program

---
+++ Bank.java
@@ -0,0 +1,15 @@
+class Bank
+{
+final int intrest_rate=4;
+final int minimum_balance=2000;
+
+
+void deposit()
+{
+System.out.println("Deposit");
+}
+void withdraw()
+{
+System.out.println("Withdraw");
+}
+}
