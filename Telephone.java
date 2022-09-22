4524fc5d7e0d90cf23998df94f5ffc06ae89300a
Krishnaveeran <dhlipkrishna@gmail.com>
2022-09-22 07:14:34 +0530

    Inheritance

---
+++ Telephone.java
@@ -0,0 +1,20 @@
+package chennai.myhome;
+
+public class Telephone
+{
+public static void main(String[] args)
+{
+Telephone ppp= new Telephone();
+ppp.make_calls();
+ppp.receive_calls();
+}
+public void make_calls()
+{
+System.out.println("Call to friends");
+}
+public void receive_calls()
+{
+System.out.println("Receive calls from friends");
+}
+}
+
