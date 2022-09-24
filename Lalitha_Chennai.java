e6d5510e2da8341c947dbcc2f8594f853ad36b13
Krishnaveeran <dhlipkrishna@gmail.com>
2022-09-24 20:42:03 +0530

    INHERITANCE

---
+++ Lalitha_Chennai.java
@@ -0,0 +1,13 @@
+class Lalitha_Chennai
+{
+int offer=500;
+public Lalitha_Chennai()
+{
+System.out.println("Samy photo claender");
+}
+public Lalitha_Chennai(int offer)
+{
+this.offer=offer;
+System.out.println("chennai offer:" +this.offer);
+}
+}
