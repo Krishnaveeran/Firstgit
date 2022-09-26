13eaee0fe4f3bc0c41ac6fde8673cde266405d6e
Krishnaveeran <dhlipkrishna@gmail.com>
2022-09-26 07:27:16 +0530

    COnstructor and static method 2

---
+++ Superclass.java
@@ -0,0 +1,15 @@
+package inherit.staticmethod;
+//javac -d . SuperClass.java
+class SuperClass
+{
+    //Creating static method
+    static void Superstaticmethod()
+    {
+     System.out.println("In static method of SupClass");
+    }
+    //Declaring Construction method
+    SuperClass()
+    {
+     System.out.println("In SupClass constructor");
+    }
+}
