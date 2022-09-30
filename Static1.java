cf69039983e28ab35aa59ecc04d2f2bf47212381
Krishnaveeran <dhlipkrishna@gmail.com>
2022-09-30 07:51:34 +0530

    Static Block Program 2

---
+++ Static1.java
@@ -0,0 +1,21 @@
+class Static1
+{
+static String name;
+static int age;
+
+static
+{
+private name="krishna";
+age=24;
+}
+static
+{
+name="krish";
+age=23;
+}
+public static void main(String [] args)
+{
+System.out.println("My name is: " +name);
+System.out.println("My age is: " +age);
+}
+}
