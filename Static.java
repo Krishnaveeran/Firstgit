2c376932cef6f999a088796766478bb769501b3a
Krishnaveeran <dhlipkrishna@gmail.com>
2022-09-30 07:49:37 +0530

    Origin master

---
+++ Static.java
@@ -0,0 +1,15 @@
+public class Static{
+
+static{
+System.out.println("This is static Block 1");
+}
+static{
+System.out.println("This is static Block 2");
+}
+static{
+System.out.println("This is static Block 3");
+}
+public static void main (String[] args)
+{
+}
+}
