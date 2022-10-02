53d3b641120def0ad32556165c89b4c555723f8a
Krishnaveeran <dhlipkrishna@gmail.com>
2022-10-02 10:36:42 +0530

    Abstract 3

---
+++ Infant.java
@@ -0,0 +1,22 @@
+ class Infant extends Child
+{
+public static void main(String[] args)
+{
+Infant Inf= new Infant();
+Inf.study();
+Inf.play();
+Inf.sing();
+}
+void sing()
+{
+System.out.println("Sing a song");
+}
+void study()
+{
+System.out.println(" Degree"); 
+}
+void play()
+{
+System.out.println("Play kho-kho game");
+}
+}
