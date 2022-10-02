997a9c3658b254e6a46c2fdb440c868787d5671c
Krishnaveeran <dhlipkrishna@gmail.com>
2022-10-02 10:35:50 +0530

    Abstract 2

---
+++ Child.java
@@ -0,0 +1,20 @@
+abstract class Child extends Parent
+{
+
+public static void main(String[] args)
+{
+//Child ch = new Child(); 
+//ch.eat(); 
+//ch.work(); 
+//ch.study(); 
+//ch.play();
+}
+//void study()
+//{
+//System.out.println(" Degree"); 
+//}
+//void play()
+//{
+//System.out.println("Play kho-kho game");
+//}
+}
