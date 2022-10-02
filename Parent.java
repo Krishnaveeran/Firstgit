60ca2bff256f82819b85444867eb8dc4735bf6dc
Krishnaveeran <dhlipkrishna@gmail.com>
2022-10-02 10:34:43 +0530

    Abstract 1

---
+++ Parent.java
@@ -0,0 +1,18 @@
+abstract class Parent
+{
+public static void main(String[] args)
+{
+System.out.println("Hi"); 
+}
+void eat()
+{
+System.out.println("Eating"); 
+}
+void work()
+{
+System.out.println("Working "); 
+}
+abstract void study();  
+ 
+
+}
